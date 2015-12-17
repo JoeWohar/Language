8ce705e9b04f4bf277682828c46188f4fe361e80
Joe <>
2015-12-17 15:47:19 +0000

    {/RajSampleProj/src/main/java/org/rajorgunit/rajsample/rajsampleproj/Customer.java}

--- RajSampleProj/src/main/java/org/rajorgunit/rajsample/rajsampleproj/Customer.java
+++ RajSampleProj/src/main/java/org/rajorgunit/rajsample/rajsampleproj/Customer.java
@@ -9,7 +9,6 @@ public class Customer implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   @org.kie.api.definition.type.Description("Raj")
    private java.lang.String firstName;
    private java.lang.String lastName;
 
